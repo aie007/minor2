8fd32a39b7b37347cda41a5687470706744b1783
aieshah <aieshah_nasir@hotmail.com>
2023-03-10 21:31:01 +0530

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import {format} from 'date-fns';
+
+import './App.css';
+
+const baseUrl = "http://localhost:5000";
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
