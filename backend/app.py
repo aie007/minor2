b83683a11addd3abe0c1eb2ed2fda8cf35930ba8
aieshah <aieshah_nasir@hotmail.com>
2023-04-05 17:47:48 +0530

    add routes

--- backend/app.py
+++ backend/app.py
@@ -127,51 +127,56 @@ class JWTTokenBlocklist(db.Model):
 class Teacher(db.Model):
     __tablename__ = 'teacher'
     tid = db.Column(db.Integer, db.ForeignKey('user.userid'), primary_key=True)
-    fname = db.Column(db.String, nullable=False)
-    lname = db.Column(db.String, nullable=False)
-    dept = db.Column(db.String, nullable=False)
-    qualification = db.Column(db.String, nullable=False)
-    institute = db.Column(db.String, nullable=False)
-    linkedin_url = db.Column(db.String, nullable=False)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
+    dept = db.Column(db.String)
+    qualification = db.Column(db.String)
+    institute = db.Column(db.String)
+    linkedin_url = db.Column(db.String)
     students = db.relationship('Student', backref='teacher', lazy=True)
 
     def __repr__(self):
         return f"Teacher: {self.fname}"
     
-    def __init__(self, fname, lname, dept, qualification, institute, linkedin_url):
-        self.fname = fname
-        self.lname = lname
-        self.dept = dept
-        self.qualification = qualification
-        self.institute = institute
-        self.linkedin_url = linkedin_url 
+    def __init__(self, tid):
+        self.tid = tid
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
+    def toDICT(self):
+        cls_dict = {}
+        cls_dict['userid'] = self.tid
+        cls_dict['fname'] = self.fname
+        cls_dict['lname'] = self.lname
+        cls_dict['dept'] = self.dept
+        cls_dict['institute'] = self.institute
+        cls_dict['qualification'] = self.qualification
+        cls_dict['linkedin_url'] = self.linkedin_url
+
+        return cls_dict
+
+    def toJSON(self):
+        return self.toDICT()
+
 class Student(db.Model):
     __tablename__ = 'student'
     sid = db.Column(db.Integer, db.ForeignKey('user.userid'), primary_key=True)
-    fname = db.Column(db.String, nullable=False)
-    lname = db.Column(db.String, nullable=False)
-    year_admitted = db.Column(db.Integer, nullable=False)
-    institute = db.Column(db.String, nullable=False)
-    enrollment_id = db.Column(db.String, unique=True, nullable=False)
-    linkedin_url = db.Column(db.String, nullable=False)
-    tid = db.Column(db.Integer, db.ForeignKey('teacher.tid'), nullable=False)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
+    year_admitted = db.Column(db.Integer)
+    institute = db.Column(db.String)
+    enrollment_id = db.Column(db.String, unique=True)
+    linkedin_url = db.Column(db.String)
+    tid = db.Column(db.Integer, db.ForeignKey('teacher.tid'))
     certificates = db.relationship('Certificate', backref='student', lazy=True)
 
     def __repr__(self):
         return f"Student: {self.fname}"
     
-    def __init__(self, fname, lname, year_admitted, institute, enrollment_id, linkedin_url):
-        self.fname = fname
-        self.lname = lname
-        self.year_admitted = year_admitted
-        self.enrollment_id = enrollment_id
-        self.institute = institute
-        self.linkedin_url = linkedin_url 
+    def __init__(self, sid):
+        self.sid = sid
 
     def save(self):
         db.session.add(self)
@@ -180,12 +185,29 @@ class Student(db.Model):
     @classmethod
     def get_by_year(cls, year):
         return cls.query.filter_by(year_admitted=year)
+    
+    def toDICT(self):
+        cls_dict = {}
+        cls_dict['userid'] = self.sid
+        cls_dict['fname'] = self.fname
+        cls_dict['lname'] = self.lname
+        cls_dict['institute'] = self.institute
+        cls_dict['year_admitted'] = self.year_admitted
+        cls_dict['enrollment_id'] = self.enrollment_id
+        cls_dict['linkedin_url'] = self.linkedin_url
+        cls_dict['tid'] = self.tid
+
+        return cls_dict
+
+    def toJSON(self):
+        return self.toDICT()
 
 class Certificate(db.Model):
     __tablename__ = 'certificate'
     certid = db.Column(db.Integer, primary_key=True, autoincrement = True)
     sid = db.Column(db.Integer, db.ForeignKey('student.sid'), nullable=False)
     certname = db.Column(db.String, nullable=False)
+    certurl = db.Column(db.String, nullable=False)
     domain = db.Column(db.String, nullable=False)
     issued_by = db.Column(db.String, nullable=False)
     issued_on = db.Column(db.Date, nullable=False)
@@ -194,8 +216,9 @@ class Certificate(db.Model):
     def __repr__(self):
         return f"Certificate: {self.certname}"
     
-    def __init__(self, certname, domain, issued_by, issued_on, validity):
+    def __init__(self, certname, certurl, domain, issued_by, issued_on, validity):
         self.certname = certname 
+        self.certurl = certurl 
         self.domain = domain
         self.issued_by = issued_by
         self.issued_on = issued_on
@@ -204,6 +227,21 @@ class Certificate(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
+
+    def toDICT(self):
+        cls_dict = {}
+        cls_dict['userid'] = self.sid
+        cls_dict['certname'] = self.certname
+        cls_dict['certurl'] = self.certurl
+        cls_dict['domain'] = self.domain
+        cls_dict['issued_by'] = self.issued_by
+        cls_dict['issued_on'] = self.issued_on
+        cls_dict['validity'] = self.validity
+
+        return cls_dict
+
+    def toJSON(self):
+        return self.toDICT()
 
 class Tag(db.Model):
     __tablename__ = 'tag'
@@ -227,6 +265,7 @@ class Tag(db.Model):
         return cls.query.filter_by(name=name).first()
 
 @app.route("/home")
+@jwt_required()
 def hello():
     response_body = {
         "name": "Nagato",
@@ -239,44 +278,14 @@ def hello():
 def create_token():
     print(request.is_json)
     data = request.get_json()
-    # print(data)
-    # print(type(data))
-    # return data
-    # print(json.dumps(data))
-    # return json.dumps(data)
-    # print(data)
     email = data["email"]
-    password = data["password"]
+    password = data["pwd"]
     if email != "test" or password != "test":
         return {"msg": "Wrong email or password"}, 401
 
     access_token = create_access_token(identity=email)
     response = {"access_token":access_token}
     return response
-
-# def token_required(f):
-#     @wraps(f)
-#     def decorator(*args, **kwargs):
-#         token = None
-#         if "authorization" in request.headers:
-#             token = request.headers["authorization"]
-#         if not token:
-#             return {"success": False, "msg": "Valid JWT token is missing"}, 400
-#         try:
-#             data = jwt.decode(token, BaseConfig.SECRET_KEY, algorithms=["HS256"])
-#             current_user = User.get_by_email(data["email"])
-#             if not current_user:
-#                 return {"success": False,
-#                         "msg": "Sorry. Wrong auth token. This user does not exist."}, 400
-#             token_expired = db.session.query(JWTTokenBlocklist.id).filter_by(jwt_token=token).scalar()
-#             if token_expired is not None:
-#                 return {"success": False, "msg": "Token revoked."}, 400
-#             if not current_user.check_jwt_auth_active():
-#                 return {"success": False, "msg": "Token expired."}, 400
-#         except:
-#             return {"success": False, "msg": "Token is invalid"}, 400
-#         return f(current_user, *args, **kwargs)
-#     return decorator
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -307,10 +316,17 @@ def register():
 
         new_user.save()
 
-        return {"success": True,
-                "userid": new_user.userid,
-                "msg": "The user was successfully registered"}, 200
+        if type == 'student':
+            new_student = Student(sid=new_user.userid)
+            return {"success": True,
+                    "userid": new_user.userid,
+                    "msg": "The student was successfully registered"}, 200
 
+        elif type == 'teacher':
+            new_teacher = Teacher(tid=new_user.userid)
+            return {"success": True,
+                    "userid": new_user.userid,
+                    "msg": "The teacher was successfully registered"}, 200
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -329,7 +345,7 @@ def login():
             return {"success": False,
                     "msg": "Invalid credentials"}, 400
     
-    token = create_access_token(identity=email)
+    token = create_access_token(identity=user_exists.userid)
     user_exists.set_jwt_auth_active(True)
     user_exists.save()
 
@@ -339,31 +355,174 @@ def login():
 
 
 @app.route("/logout", methods=["POST"])
+@jwt_required()
 def logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
 
-# @app.route('/updateStudent', methods=['POST','GET'])
-# @jwt_required
-# def updateS(self):
-#     if request.method == 'POST':
-#         req_data = request.get_json()
+@app.route('/updateStudent', methods=['POST','GET'])
+@jwt_required()
+def updateS():
+    if request.method == 'POST':
+        req_data = request.get_json()
 
-#         fname = req_data.get("fname")
-#         lname = req_data.get("lname")
-#         year_admitted = req_data.get("year_admitted")
-#         institute = req_data.get("institute")
-#         enrollment_id = req_data.get("enrollment_id")
-#         linkedin_url = req_data.get("linkedin_url")
+        fname = req_data.get("fname")
+        lname = req_data.get("lname")
+        year_admitted = req_data.get("year_admitted")
+        institute = req_data.get("institute")
+        enrollment_id = req_data.get("enrollment_id")
+        linkedin_url = req_data.get("linkedin_url")
+        tid = req_data.get("tid")
 
-#         if not fname or not lname or not year_admitted or not institute or not enrollment_id or not linkedin_url:
-#             return {"success": False,
-#                     "msg": "Invalid input"}, 400
+        if not fname or not lname or not year_admitted or not institute or not enrollment_id or not linkedin_url or not tid:
+            return {"success": False,
+                    "msg": "Invalid input"}, 400
         
-        
+        usernow = Student.query.filter_by(sid=get_jwt_identity).first()
+        if not usernow:
+            return {"success": False,
+                    "msg": "Student does not exist"}, 400
 
-#     else:
+        usernow.fname = fname
+        usernow.lname = lname
+        usernow.year_admitted = year_admitted
+        usernow.institute = institute
+        usernow.enrollment_id = enrollment_id
+        usernow.linkedin_url = linkedin_url
+        usernow.tid = tid
+
+        usernow.save()
+
+        return {"success": True,
+                "msg": "Student profile updated successfully"}, 200
+        
+    else:
+        usernow = Student.query.filter_by(sid=get_jwt_identity).first()
+        if not usernow:
+            return {"success": False,
+                    "msg": "Student does not exist"}, 400
+        
+        avbl_teachers = jsonify(Teacher.query.all())
+
+        return {"success": True,
+                "user": usernow.toJSON(),
+                "teachers": avbl_teachers}, 200
+
+@app.route('/updateTeacher', methods=['POST','GET'])
+@jwt_required()
+def updateT():
+    if request.method == 'POST':
+        req_data = request.get_json()
+
+        fname = req_data.get("fname")
+        lname = req_data.get("lname")
+        dept = req_data.get("dept")
+        institute = req_data.get("institute")
+        qualification = req_data.get("qualification")
+        linkedin_url = req_data.get("linkedin_url")
+
+        if not fname or not lname or not dept or not institute or not qualification or not linkedin_url:
+            return {"success": False,
+                    "msg": "Invalid input"}, 400
+        
+        usernow = Teacher.query.filter_by(tid=get_jwt_identity).first()
+        if not usernow:
+            return {"success": False,
+                    "msg": "Teacher does not exist"}, 400
+
+        usernow.fname = fname
+        usernow.lname = lname
+        usernow.dept = dept
+        usernow.institute = institute
+        usernow.qualification = qualification
+        usernow.linkedin_url = linkedin_url
+
+        usernow.save()
+
+        return {"success": True,
+                "msg": "Teacher profile updated successfully"}, 200
+        
+    else:
+        usernow = Teacher.query.filter_by(tid=get_jwt_identity).first()
+        if not usernow:
+            return {"success": False,
+                    "msg": "Teacher does not exist"}, 400
+        
+        return {"success": True,
+                "user": usernow.toJSON()}, 200
+
+@app.route('/upload', methods=['POST'])
+@jwt_required()
+def uploadcert():
+    usernow = Student.query.filter_by(sid=get_jwt_identity).first()
+    if not usernow:
+        return {"success": False,
+                "msg": "Student does not exist"}, 400
+    
+    req_data = request.get_json
+
+    certname = req_data.get("certname")
+    domain = req_data.get("domain")
+    issued_by = req_data.get("issued_by")
+    issued_on = req_data.get("issued_on")
+    validity = req_data.get("validity")
+
+    if not certname or not domain or not issued_by or not issued_on or not validity:
+        return {"success": False,
+                "msg": "Invalid input"}, 400
+
+    domains = Tag.quer.filter_by(type="domain").all()
+    issued_bys = Tag.query.filter_by(type="issued_by").all()
+    if not domains or not issued_bys or domain not in domains or issued_by not in issued_bys:
+        return {"success": False,
+                "msg": "Requested tag not available. Ask your teacher to create one!"}, 400
+
+    # aws s3 code to get url 
+
+    new_certificate = Certificate(certname=certname, certurl="xyz.com", domain=domain, issued_by=issued_by, issued_on=issued_on, validity=validity)
+    
+    new_certificate.save()
+
+    return {"success": True,
+            "msg": "Certificate uploaded", 
+            "certificate": new_certificate.toJSON()}, 200
+
+@app.route('/certificates', methods=['GET'])
+@jwt_required()
+def displaycert():
+    # assume user is a student
+    usernow = Student.query.filter_by(sid=get_jwt_identity).first()
+
+    # user is not a student
+    if not usernow:
+        # assume user is a teacher
+        usernow = Teacher.query.filter_by(tid=get_jwt_identity).first()
+        if not usernow:
+            return {"success": False,
+                    "msg": "Teacher does not exist"}, 400
+        
+        allCertificates = []
+        
+        for sid in usernow.students:
+            allCertificates.append(Certificate.query.filter_by(sid=sid).all())
+
+        if not allCertificates:
+            return {"success": True,
+                    "msg": "No certificate to display"}, 200
+        
+        return {"success": True,
+                "msg": "Certificates available",
+                "certificates": jsonify(allCertificates)}, 200
+    
+    allCertificates = Certificate.query.filter_by(sid=usernow.sid).all()
+    if not allCertificates:
+        return {"success": True,
+                "msg": "No certificate to display. Come back after uploading some certificate!"}, 200
+    
+    return {"success": True,
+            "msg": "Certificates available",
+            "certificates": jsonify(allCertificates)}, 200
 
 if __name__ == '__main__':
     app.run()
